daa5627f20edb7d60c89913a89606808c2c43b65
Unknown <crystian.perafan@correounivalle.edu.co>
2017-05-19 17:47:54 -0500

    Se agrego el paquete control
    Se agrega paquete control y se esta haciendo conexion con la base de datos

---
+++ taller4/src/control/controladora.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+
+import modelo.*;
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Statement;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author invitado
+ */
+public class controladora {
+    
+    private static String DRIVER = "com.mysql.jdbc.Driver";
+    
+    private static Connection conexion;
+    
+    private static Statement consulta;
+    
+    
+    
+    /**
+     * Inicia la base de datos
+     */
+    public static void iniciarDB(){
+        try{
+            Class.forName(DRIVER).newInstance();
+            
+        }catch(ClassNotFoundException | IllegalAccessException | InstantiationException e){
+            JOptionPane.showMessageDialog(null, "Error al cargar el Controlador");
+        }
+    }
+    
+    public static void main(String args[]){
+        
+    }
+
+}
